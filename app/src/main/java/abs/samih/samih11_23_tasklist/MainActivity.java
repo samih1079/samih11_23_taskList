cd67f01776f22830ea12055865473514fc592642
Admin <samih1079@gmail.com>
2023-11-19 10:41:53 +0200

    Initial commit

---
+++ app/src/main/java/abs/samih/samih11_23_tasklist/MainActivity.java
@@ -0,0 +1,14 @@
+package abs.samih.samih11_23_tasklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
